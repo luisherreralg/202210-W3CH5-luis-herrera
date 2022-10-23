68af621a4efb77e4edf0fd6f1cb1228029cfa986
luisherreralg <lherreralg@outlook.es>
2022-10-23 22:50:27 +0200

    added new changes

--- src/index.details.ts
+++ src/index.details.ts
@@ -2,6 +2,7 @@ import { Header } from './components/header.js';
 import { PokeDetailPrint } from './components/poke.print.details.js';
 
 new Header('body');
+
 new PokeDetailPrint(
   'section.section',
   `https://pokeapi.co/api/v2/pokemon/${localStorage.getItem('PokeClick')}`
